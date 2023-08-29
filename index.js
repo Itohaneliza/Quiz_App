403af0012799b7df3ce927e7b3fc9d18c8132461
Itohaneliza <juliuseli2004@gmail.com>
2023-08-29 11:44:39 +0100

    quiz app

---
+++ index.js
@@ -0,0 +1,46 @@
+export default [
+{
+    question: "What is my favourite food?",
+    answers: [
+        {option: "Rice and plantain", correct: false},
+        {option: "Fufu and okra soup", correct: true},
+        {option: "Beans and corn", correct: false},
+    ],
+},
+
+{
+    question: "What do you think is my favourite color?",
+    answers: [
+        {option: "Red", correct: false},
+        {option: "Black", correct: true},
+        {option: "Purple", correct: false},
+    ],
+},
+
+{
+    question: "Where did I get my first degree?",
+    answers: [
+        {option: "Funnab", correct: false},
+        {option: "Unilag", correct: true},
+        {option: "Uniben", correct: false},
+    ],
+},
+
+{
+    question: "What is my surname?",
+    answers: [
+        {option: "Okonofua", correct: true},
+        {option: "Julius", correct: false},
+        {option: "Itohan", correct: false},
+    ],
+},
+
+{
+    question: "How many piercings do I have",
+    answers: [
+        {option: "6", correct: true},
+        {option: "7", correct: false},
+        {option: "2", correct: false},
+    ],
+},
+]
